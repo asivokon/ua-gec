532ecec3d8a524c3cc8017840c2d0c931b593b4c
Oleksiy Syvokon <oleksiy.syvokon@gmail.com>
2022-04-16 17:40:17 +0300

    Skip files where number of sentences is different
    This is a dataset bug that should be fixed

--- baselines/bart/prepare_data.py
+++ baselines/bart/prepare_data.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python3
 import argparse
-import fileinput
 import glob
 import json
 
 
 def prepare_dataset(data_dir, valid_size=3000):
-    src_files = glob.glob(f"{data_dir}/train/source-sentences/*")
-    tgt_files = glob.glob(f"{data_dir}/train/target-sentences/*")
-    for s, t in zip(src_files, tgt_files):
-        with open(s) as f:
-            s_lines = f.readlines()
-        with open(t) as f:
-            t_lines = f.readlines()
-
-        if len(s_lines) != len(t_lines):
-            print(s)
-
-
-
-    src_lines = list(fileinput.input(src_files))
-    tgt_lines = list(fileinput.input(tgt_files))
-    print(len(src_files))
-    print(len(tgt_files))
-    assert len(src_lines) == len(tgt_lines)
+    src_lines, tgt_lines = read_parallel(data_dir)
 
     src_train = src_lines[:-valid_size]
     tgt_train = tgt_lines[:-valid_size]
@@ -33,6 +15,30 @@ def prepare_dataset(data_dir, valid_size=3000):
     tgt_valid = tgt_lines[-valid_size:]
     write_jsonlines(src_valid, tgt_valid, "valid.json")
 
+
+def read_parallel(data_dir):
+    src_files = glob.glob(f"{data_dir}/train/source-sentences/*.src.txt")
+    tgt_files = glob.glob(f"{data_dir}/train/target-sentences/*.a1.txt")
+    src_files.sort()
+    tgt_files.sort()
+    src_lines = []
+    tgt_lines = []
+
+    for s, t in zip(src_files, tgt_files):
+        with open(s) as f:
+            s_lines = f.readlines()
+        with open(t) as f:
+            t_lines = f.readlines()
+
+        if len(s_lines) == len(t_lines):
+            src_lines += s_lines
+            tgt_lines += t_lines
+        else:
+            print(f"Skipping {s} ({len(s_lines)} lines in source, "
+                  f"{len(t_lines)} in target)")
+
+    assert len(src_lines) == len(tgt_lines)
+    return src_lines, tgt_lines
 
 def write_jsonlines(src_lines, tgt_lines, out_path):
     with open(out_path, "wt") as f_out:
